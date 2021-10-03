20b4da888734cc949cdcaed233b2151a80a06388
Lucia Campos <camposluciabelen@gmail.com.ar>
2021-10-03 20:39:02 -0300

    subirdesafio

---
+++ main.js
@@ -0,0 +1,18 @@
+let nombre = prompt ("Ingrese su nombre")
+
+const espacio = (" ")
+
+let hola = "¡Hola" + espacio + nombre + "!"
+
+console.log (hola)
+
+let diadehoy = prompt("¿Qué día es hoy");
+
+let mes = prompt ("¿En qué mes estamos?");
+
+let año = "-2021"
+
+alert (diadehoy + "-" + mes + año);
+
+console.log (diadehoy + "-" + mes + año)
+
